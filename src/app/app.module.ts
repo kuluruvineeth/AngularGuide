20fb2dac938acf17feb5e3e71f79583a69eadd9e
kuluruvineeth <kuluruvineeth8623@gmail.com>
2022-07-10 13:55:55 +0530

    finished product row and added subcomponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
+import { ProductImageComponent } from './product-image/product-image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    ProductListComponent
+    ProductListComponent,
+    PriceDisplayComponent,
+    ProductDepartmentComponent,
+    ProductImageComponent
   ],
   imports: [
     BrowserModule
